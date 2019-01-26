1c9018091da14f1353d782523aa4b01b9fc213e0
Alexander Raphael Kahl <VCL2311@uniqa.at>
2019-01-26 14:47:56 +0100

    setting up the package structure

---
+++ settings/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@title: raphka package: Equity Performance and Riskattribution 
+@author: Alexander Kahl
+@date: Tue Sep  11 09:29:10 2018
+@version: 0.01
+@description: this package contains all classes, functionality and settings
+              to be used in all python projects and tools
+@Details:     The goal is to have package that contains all version controlled
+              tools and key functionality used within raphka
+@external file paths: No external file paths shall be used! 
+		      For simplicity an excel file sheet serves as stock database
+@key inputs:
+@key variables:
+@external sources:
+@key output:
+@comment:     As of now pyquant package includes three subpackages. Classes,
+              database and settings. This is only the early stage of
+              development  and will be extended tremendously.
+"""
+
+from rahka.settings import *
